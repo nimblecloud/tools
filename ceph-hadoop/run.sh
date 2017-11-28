f955ce37f6e5ef4d52fb52d4106475cef45f4ebc
zhangwenping <isware@126.com>
2017-11-28 10:33:50 +0800

    add ceph hadoop test code

---
+++ ceph-hadoop/run.sh
@@ -0,0 +1,5 @@
+export CLASSPATH=/usr/hdp/2.6.2.0-205/hadoop/*:/usr/hdp/2.6.2.0-205/hadoop/lib/*:$CLASSPATH
+#export CLASSPATH=/usr/share/java/libcephfs.jar:/usr/share/java/hadoop-cephfs.jar:$CLASSPATH
+export CLASSPATH=.:$CLASSPATH
+
+java HelloWorld
